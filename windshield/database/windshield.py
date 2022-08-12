11e1543e0e0b2712c6a476bbbef005d086c33715
Simona Cordos <cordos.simona@gmail.com>
2022-08-12 21:27:28 +0300

    add Database class, add search-eurocode command

--- windshield/database/windshield.py
+++ windshield/database/windshield.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("windshield")
 
 
 @dataclass
-class WindshieldData:
+class WindshieldCreateData:
     brand: str
     model: str
     year: int
@@ -16,14 +16,56 @@ class WindshieldData:
     heat: bool
     eurocode: str
 
+@dataclass
+class WindshieldSearchData:
+    brand: str
+    model: str
+    year: int
+    sensor: bool
+    camera: bool
+    heat: bool
+    
 
-def create_windshield(data: WindshieldData, connection: sqlite3.Connection) -> None:
-    """adauga in baza de date un windshield nou"""
-    cursor = connection.cursor()
-    cursor.execute("""
-    INSERT INTO windshields (brand, model, year, sensor, camera, heat, eurocode) 
-    VALUES (?, ?, ?, ?, ?, ?, ?);
-    """, (data.brand, data.model, data.year, data.sensor, data.camera, data.heat, data.eurocode,))
-    connection.commit()
-    logger.info("Windshield inserted into database.")
-      
+class EurocodeExists(Exception):
+    pass 
+
+
+class Database:
+
+    def __init__(self, connection: sqlite3.Connection) -> None:
+        self._connection = connection    
+
+    def create_windshield(self, data: WindshieldCreateData) -> None:
+        """adauga in baza de date un windshield nou"""
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute("""
+            INSERT INTO windshields (brand, model, year, sensor, camera, heat, eurocode) 
+            VALUES (?, ?, ?, ?, ?, ?, ?);
+            """, (data.brand, data.model, data.year, data.sensor, data.camera, data.heat, data.eurocode,))
+            self._connection.commit()
+        except sqlite3.IntegrityError:
+            raise EurocodeExists()     
+        logger.info("Windshield inserted into database.")
+
+    def search_eurocode(self, data: WindshieldSearchData) -> str | None:
+        cursor = self._connection.cursor()
+        result = cursor.execute("""SELECT eurocode FROM windshields 
+        WHERE brand = ? and model = ? and year = ? and sensor = ? and camera = ? and heat = ?;
+        """, (data.brand, data.model, data.year, data.sensor, data.camera, data.heat,))
+        row = result.fetchone()
+        if row is None:
+            return None
+        return row[0]    
+
+        
+        
+
+
+
+
+
+
+
+
+        
--- windshield/main.py
+++ windshield/main.py
@@ -3,7 +3,7 @@ import sys
 import sqlite3
 import pathlib
 import logging
-from windshield.database.windshield import WindshieldData, create_windshield
+from windshield.database.windshield import Database, EurocodeExists, WindshieldCreateData, WindshieldSearchData
 
 
 logger = logging.getLogger("windshield")
@@ -12,32 +12,58 @@ cli_handler.setLevel(logging.INFO)
 logger.addHandler(cli_handler)
 
 
-
 ROOT = pathlib.Path(__file__).parent.parent
 DB_FILE = ROOT.joinpath("windshield.db")
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    # command este numele variabilei, create-windshield/search-eurocode sunt valorile pt command. 
     subparsers = parser.add_subparsers(dest="command",required=True)
+    # se creaza comanda create-winshield:
     create_parser = subparsers.add_parser("create-windshield")
+    # se creaza argumentele pt comanda create-windshield.
     create_parser.add_argument("--brand", required=True)
-    create_parser.add_argument("--model", required=True)
+    create_parser.add_argument("--model", required=True, help="modelul autoturismului")
     create_parser.add_argument("--year", required=True, type=int, help="an intre 1960 si anul curent")
     create_parser.add_argument("--sensor", action="store_true")
     create_parser.add_argument("--camera", action="store_true")
     create_parser.add_argument("--heat", action="store_true")
     create_parser.add_argument("--eurocode", required=True)
+
+    # se creaza comanda search-eurocode:
+    search_parser = subparsers.add_parser("search-eurocode")
+    # se creaza argumentele pt comanda search-eurocode.
+    search_parser.add_argument("--brand", required=True)
+    search_parser.add_argument("--model", required=True, help="modelul autoturismului")
+    search_parser.add_argument("--year", required=True, type=int, help="an intre 1960 si anul curent")
+    search_parser.add_argument("--sensor", action="store_true")
+    search_parser.add_argument("--camera", action="store_true")
+    search_parser.add_argument("--heat", action="store_true")
+
     return parser.parse_args(sys.argv[1:])
 
 
 def main(): 
     with sqlite3.connect(DB_FILE) as connection:
+        database = Database(connection)
         args = parse_arguments()
         if args.command == "create-windshield":
             logger.info("User has selected create windshield command.")
-            windshield_data = WindshieldData(args.brand, args.model, args.year, args.sensor, args.camera, args.heat, args.eurocode)    
-            create_windshield(windshield_data, connection)
+            windshield_create_data = WindshieldCreateData(args.brand, args.model, args.year, args.sensor, args.camera, args.heat, args.eurocode) 
+            try:   
+                database.create_windshield(windshield_create_data)
+            except EurocodeExists:
+                print("Eurocode already exists.")   
+            else:
+                print("Windshield successfully created.") 
+        elif args.command == "search-eurocode":
+            windshield_search_data = WindshieldSearchData(args.brand, args.model, args.year, args.sensor, args.camera, args.heat)
+            eurocode = database.search_eurocode(windshield_search_data)
+            if eurocode is None:
+                print("Eurocode not found.")
+            else:    
+                print(f"Eurocode for windshield is {eurocode}")        
 
 
 if __name__ == "__main__": 
